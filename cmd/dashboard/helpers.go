6c35611efe92ec7232975b3f14d2f97c22148b76
adel <65723807+theadell@users.noreply.github.com>
2023-10-15 11:54:05 +0200

    helpers for rendering and errors

--- cmd/dashboard/helpers.go
+++ cmd/dashboard/helpers.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rand"
 	"crypto/sha256"
 	"encoding/base64"
+	"errors"
+	"fmt"
+	"log/slog"
 	"net"
+	"net/http"
 	"regexp"
+	"runtime/debug"
 	"strconv"
 )
 
@@ -65,4 +71,54 @@ func stringToUint(s string) (uint, error) {
 
 	// Convert uint64 to uint
 	return uint(u64), nil
+}
+func validateRecordReq(hostname, ip, ttl, recordType string) error {
+	if !isValidType(recordType) {
+		return errors.New("Invalid record type")
+	}
+	if !isValidHostname(hostname) {
+		return errors.New("Invalid hostname")
+	}
+	if recordType == "A" && !isValidIPv4(ip) || recordType == "AAAA" && !isValidIPv6(ip) {
+		return errors.New("Invalid IP address")
+	}
+	if !isValidTTL(ttl) {
+		return errors.New("Invalid ttl")
+	}
+	return nil
+}
+func httpError(w http.ResponseWriter, message string, code int) {
+	slog.Error(message)
+	http.Error(w, message, code)
+}
+
+func (app *App) render(w http.ResponseWriter, status int, page string, data any) {
+	ts, ok := app.templateCache[page]
+	if !ok {
+		err := fmt.Errorf("the template %s does not exist", page)
+		app.serverError(w, err)
+		return
+	}
+
+	buf := new(bytes.Buffer)
+
+	if err := ts.Execute(buf, data); err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(status)
+
+	// Write the buffer content to the response writer
+	buf.WriteTo(w)
+}
+func (app *App) serverError(w http.ResponseWriter, err error) {
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+
+	slog.Error("server error encountered",
+		"error", err.Error(),
+		"trace", trace,
+	)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
