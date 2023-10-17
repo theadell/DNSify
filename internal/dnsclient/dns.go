8dcc99da4f12cb8e3e0943c991a1f56b51814b1b
adel <65723807+theadell@users.noreply.github.com>
2023-10-17 12:59:53 +0200

    method for fetching records using zone transfer

--- internal/dnsclient/dns.go
+++ internal/dnsclient/dns.go
@@ -285,3 +285,71 @@ func (c *BindClient) backgroundSync() {
 func (c *BindClient) Close() {
 	close(c.done)
 }
+
+// fetchZoneRecords fetches the DNS zone records for a given domain from a specific DNS server using
+// the AXFR (Authoritative Zone Transfer) method. The caller must provide TSIG (Transaction Signature)
+// authentication details using a key and a secret. Note: For this function to work, the DNS server in question
+// must have zone transfers enabled for clients that present valid TSIG keys. For instance, using BIND9 you must include
+// the below directive to your configuration
+// allow-transfer { key "key-name"; };
+//
+// Parameters:
+//
+// domain: The target domain name (zone), e.g., "example.com".
+//
+// dnsServer: The address (including port) of the DNS server to fetch the records from, e.g. "ns1.example.com:53".
+//
+// keyName: The name of the TSIG key used for authentication.
+//
+// secret: The actual secret part of the TSIG key. Both the DNS client and server must have this
+//
+//	secret to mutually authenticate DNS messages.
+//
+// Returns:
+//
+// If successful, returns a slice of DNS records related to the domain. If there are any errors during the
+// process, the function returns an error.
+func fetchZoneRecords(domain, dnsServer, keyName, secret string) ([]Record, error) {
+	// Create a new DNS message.
+	m := new(dns.Msg)
+
+	// Create a transfer object.
+	t := new(dns.Transfer)
+
+	// Associate the keyName with the provided secret for TSIG authentication.
+	t.TsigSecret = map[string]string{keyName: secret}
+
+	// Set the request type to AXFR to fetch all records of the domain.
+	m.SetAxfr(domain)
+
+	// Set up TSIG (Transaction Signature) for authentication.
+	m.SetTsig(keyName, dns.HmacSHA256, 300, time.Now().Unix())
+
+	// Initiate the transfer.
+	channels, err := t.In(m, dnsServer)
+	if err != nil {
+		return nil, err
+	}
+
+	var records []Record
+	var rr []dns.RR
+	// Process the responses to collect records.
+	for env := range channels {
+		if env.Error != nil {
+			return nil, env.Error
+		}
+		rr = append(rr, env.RR...)
+	}
+	for _, r := range rr {
+		switch record := r.(type) {
+		case *dns.A:
+			records = append(records, NewRecord("A", record.Hdr.Name, record.A.String(), uint(record.Hdr.Ttl)))
+		case *dns.AAAA:
+			records = append(records, NewRecord("AAAA", record.Hdr.Name, record.AAAA.String(), uint(record.Hdr.Ttl)))
+		case *dns.NS:
+			records = append(records, NewRecord("NS", record.Hdr.Name, record.Ns, uint(record.Hdr.Ttl)))
+		}
+	}
+
+	return records, nil
+}
